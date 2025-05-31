b689518859ce1376fec76547b092fb9b71c07118
Benjamín Collu <benjamincollu@gmail.com>
2025-06-01 00:24:59 +0200

    finalize file modification mechanism

---
+++ README.md
@@ -0,0 +1,18 @@
+# scs-porter
+
+A command-line tool to port .scs mod files to another version of Euro Truck Simulator 2 or American Truck Simulator.
+
+## Usage
+
+```bash
+java -jar scs-porter.jar <file> <version to port to>
+```
+example:
+
+```bash
+java -jar scs-porter.jar mod.scs 1.54
+```
+
+## Release
+
+Can be downloaded from the [releases page](https://github.com/benjamincollu/scs-porter/releases), run using java -jar command as shown in the usage section.
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
@@ -6,70 +5,33 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // argument check
         if (args.length != 2) {
             System.err.println("Wrong amount of arguments, displaying help dialog...");
             helpDialog();
             System.exit(1);
         }
+
         String fileName = args[0];
         String to = args[1];
 
-        try {
-            System.out.println("Extracting manifest.sii from " + fileName + " to " + to);
-            extractFile(Path.of(fileName), Path.of("manifest.sii"));
-        } catch (IOException e) {
-            System.err.println("Error extracting manifest.sii from " + fileName + ": ");
-            e.printStackTrace();
-            System.exit(2);
-        }
-
-
+        // file operations
         try {
             System.out.println("Reading manifest.sii and setting compatible_versions to " + to + ".*");
-            changeVersion(to);
+            changeVersion(Path.of(fileName), to);
         } catch (IOException e) {
-            System.err.println("Error writing to file manifest.sii");
+            System.err.println("Error performing operations on file manifest.sii");
             e.printStackTrace();
             System.exit(2);
         }
 
-        System.out.println("Sucessfully extracted manifest.sii");
+        System.out.println("Sucessfully ported " + fileName + " to version " + to + ".*");
     }
-
-    private static void changeVersion(String to) throws IOException {
-        Scanner scanner = new Scanner(new File("manifest.sii"));
-        boolean found = false;
-        ArrayList<String> lines = new ArrayList<>();
-        int lineNumber = 0;
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine().trim();
-            lines.add(line);
-            if (line.startsWith("compatible_versions")) {
-                System.out.println("Found compatible_versions line: " + lineNumber);
-                found = true;
-                // duplicate that line and set the version to port to
-                // it looks like this compatible_versions[]: "1.53.*"
-                String newLine = "compatible_versions[]: \"" + to + ".*\" # ported by scs-porter";
-                lines.add(newLine);
-                break;
-            }
-            lineNumber++;
-        }
-        if (found) {
-            // write the lines back to manifest.sii
-            Files.write(Path.of("manifest.sii"), lines);
-            System.out.println("Successfully set compatible_versions to " + to + ".*");
-        } else {
-            System.err.println("No compatible_versions line found in manifest.sii");
-            System.exit(2);
-        }
-    }
-
     public static void helpDialog() {
         System.out.println("scs-porter - A command-line tool to port .scs mod files to another version of Euro Truck Simulator 2");
         System.out.println("valid syntax:");
@@ -78,11 +40,37 @@ public class Main {
         System.out.println("    scs-porter.jar mod.scs 1.54");
     }
 
-    private static void extractFile(Path source, Path output) throws IOException {
+    private static void changeVersion(Path source, String to) throws IOException {
         URI uri = URI.create("jar:" + source.toUri());
         try (FileSystem fileSystem = FileSystems.newFileSystem(uri, Map.of("create", "false"))) {
-            Path fileToExtract = fileSystem.getPath("/" + "manifest.sii");
-            Files.copy(fileToExtract, output);
+            Path manifest = fileSystem.getPath("/manifest.sii");
+
+            boolean found = false;
+
+            List<String> oldFile = Files.readAllLines(manifest);
+            ArrayList<String> newFile = new ArrayList<>();
+
+            int lineNumber = 0;
+
+            for (String line : oldFile) {
+                newFile.add(line);
+                if (!found && line.trim().startsWith("compatible_versions")) {
+                    System.out.println("Found compatible_versions line: " + lineNumber);
+                    found = true;
+                    newFile.add("compatible_versions[]: \"" + to + ".*\" # ported by scs-porter");
+                }
+                if (!found) {
+                    lineNumber++;
+                }
+            }
+
+            if (!found) {
+                System.err.println("No compatible_versions line found in manifest.sii");
+                System.exit(2);
+            }
+
+            Files.write(manifest, newFile);
+            System.out.println("Successfully set compatible_versions to " + to + ".*");
         }
     }
 }
